098be1da4ec8df51030266b65703017aa6ac4dae
mh <mh@immerda.ch>
2011-05-12 22:46:40 +0200

    also manage service

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,4 +2,10 @@ class haveged::base {
   package { 'haveged':
     ensure => present,
   }    
+  service{'haveged':
+    ensure => running,
+    enable => true,
+    require => Package['haveged'],
+    hasstatus => true,
+  }
 }
